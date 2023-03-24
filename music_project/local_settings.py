b00f5e8d0dee56628783185fc943ab2178110cb5
Amy Kaminski <amykaminski928@gmail.com>
2023-03-24 13:39:49 -0500

    created local_settings file + project packages installed.

---
+++ music_project/local_settings.py
@@ -0,0 +1,16 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-y0)8(c=4c&wlgb*63g_og!w6%phb@g7+xiuf*owm&ipn_o-em)'
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '0liver-El@1se'
+    }
+}
