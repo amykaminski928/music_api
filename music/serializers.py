d2bba8064f61f888a9b4ceb7fc135e466c36c113
Amy Kaminski <amykaminski928@gmail.com>
2023-03-25 23:29:11 -0500

    added Put and Delete function + added postman collection to api

--- music/serializers.py
+++ music/serializers.py
@@ -4,6 +4,6 @@ from .models import Song;
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'title', 'album', 'release_date', 'genre']
+        fields = ['id', 'title', 'artist', 'album', 'release_date', 'genre']
 
 
--- music/views.py
+++ music/views.py
@@ -4,22 +4,37 @@ from rest_framework.response import Response
 from .serializers import SongSerializer
 from .models import Song
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
-@api_view(['GET'])
-def get_all_songs (request):
-    songs = Song.objects.all()
-    serializer = SongSerializer(songs, many=True) 
-    
-    return Response (serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET', 'POST'])
+def get_all_songs(request):
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)        
+        return Response (serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def get_by_id (request, pk): 
-    try:
-        song = Song.objects.get(pk=pk)
-        serializer = SongSerializer(song)
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        try:
+            serializer = SongSerializer(song)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Song.DoesNotExist:
+            return Response(status = status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':       
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
-    except Song.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
         
